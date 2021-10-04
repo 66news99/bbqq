4dc4907195eae27fe0ee9b687972f07e0acf2d34
ui-hyeong <jwh4861230@likelion.org>
2021-10-05 01:38:27 +0900

    10/05 plan to add test

--- bbqq/examples/main_example.py
+++ bbqq/examples/main_example.py
@@ -68,21 +68,33 @@ def main():
     optimizer = optim.AdamW(params=clssfer.parameters(), lr=learning_rate)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
+    #------------------------------ train & test ---------------------------------
+
     for e_idx, epoch in enumerate(range(EPOCHS)):
         losses = list()
         train_acc = 0.0
+        test_acc = 0.0
+
+        model.train()
         for b_idx, batch in enumerate(tqdm(dataloader)):
             X, y = batch
             loss = clssfer.training_step(X, y)
+            y_hat = clssfer.forward(X)
             optimizer.zero_grad()  # resetting the gradients.
             loss.backward(retain_graph=True)  # backprop the loss
             optimizer.step()  # gradient step
-            train_acc += calc_accuracy(clssfer.forward(X), y)
+            train_acc += calc_accuracy(y_hat, y)
             losses.append(loss.item())
         avg_loss = (sum(losses) / len(losses))
         if b_idx % log_interval == 0:
             print("epoch {} batch id {} loss {} train acc {}".format(e_idx, b_idx + 1, loss.data.cpu().numpy(), train_acc / (b_idx + 1)))
     print("epoch {} train acc {}".format(EPOCHS, train_acc / (b_idx + 1)))
 
+    # model.eval()
+    # for b_idx, batch in enumerate(tqdm(dataloader)):
+    #     X, y = batch
+    #     y_hat = clssfer.forward(X)
+    #     test_acc += calc_accuracy(y_hat, y)
+    # print(f"epoch {e_idx} test acc {test_acc}" )
 if __name__ == '__main__':
     main()
